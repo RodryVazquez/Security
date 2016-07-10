05598bdf3246c07c57fddb421bac053f2150fe0e
rodrigo@rodrigovazquez.com.mx <rodrigo@rodrigovazquez.com.mx>
2016-07-10 16:53:03 -0600

    se agrego proyecto

---
+++ Securing Application using permissions/ReadSMS/app/src/main/java/com/example/rodrigo/readsms/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.rodrigo.readsms;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public class SmsColumns{
+        public static final String ID = "_id";
+        public static final String ADDRESS = "address";
+        public static final String DATE = "date";
+        public static final String BODY = "body";
+    }
+
+    private class SmaCursorAdapter{
+
+    }
+}
+
+
