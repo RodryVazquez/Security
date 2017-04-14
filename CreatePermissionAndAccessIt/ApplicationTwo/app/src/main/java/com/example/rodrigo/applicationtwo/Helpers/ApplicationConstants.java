3975b6a85fa1499d451fe349736bbef83f43b7c9
Rodrigo Vazquez <rodrigo.vazquez@delpunto.com>
2017-04-13 18:09:23 -0600

    Se agrego funcionalidad para acceder al componente de otra aplicacion atravez de un permiso

---
+++ CreatePermissionAndAccessIt/ApplicationTwo/app/src/main/java/com/example/rodrigo/applicationtwo/Helpers/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package com.example.rodrigo.applicationtwo.Helpers;
+
+
+/**
+ * Constantes
+ */
+public class ApplicationConstants {
+
+    public static final String ACTION = "Android.example.Application1";
+    public static  final String CATEGORY = "android.intent.category.DEFAULT";
+}
--- CreatePermissionAndAccessIt/ApplicationTwo/app/src/main/java/com/example/rodrigo/applicationtwo/MainActivity.java
+++ CreatePermissionAndAccessIt/ApplicationTwo/app/src/main/java/com/example/rodrigo/applicationtwo/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.rodrigo.applicationtwo;
 
+import android.app.Application;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.rodrigo.applicationtwo.Helpers.ApplicationConstants;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Referenciamos los elementos de la vista
     private Button btnTest;
     private TextView txtMessage;
     int error = 0;
     private static final int requestCode_Intent = 3;
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,40 @@ public class MainActivity extends AppCompatActivity {
         btnTest = (Button)findViewById(R.id.btnTest);
         txtMessage = (TextView) findViewById(R.id.txtMessage);
 
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Declaramos el intent para llamar ala ApplicationOne
+                Intent intent = new Intent();
+                intent.setAction(ApplicationConstants.ACTION);
+                intent.addCategory(ApplicationConstants.CATEGORY);
+                try {
+                    startActivityForResult(intent,requestCode_Intent);
+                }
+                catch (Exception ex){
+                    txtMessage.append("\n"+ex.getMessage()+"\n");
+                    Toast.makeText(MainActivity.this,R.string.error_message_permission,Toast.LENGTH_SHORT).show();
+                    error = 1;
+                }
+            }
+        });
+    }
+
+    /**
+     * Verificamos el requestCode, si fue exitoso mostramos un mensaje si no manejamos el error
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == requestCode_Intent && error == 0){
+            txtMessage.append(getString(R.string.open_application_activity_message));
+            Toast.makeText(this, R.string.open_application_message,Toast.LENGTH_SHORT).show();
+            error = 0;
+        }else{
+            /* Manejamos la excepcion o error*/
+        }
     }
 }
